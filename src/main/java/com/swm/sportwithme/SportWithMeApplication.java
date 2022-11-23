1ba9eb8888e7e701aa481b4c3a5fe1dcfe336c50
KhalezAlex <klozevitz@yandex.ru>
2022-11-23 12:34:07 +0300

    Initial commit

---
+++ src/main/java/com/swm/sportwithme/SportWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.swm.sportwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swm/sportwithme/SportWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swm.sportwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
